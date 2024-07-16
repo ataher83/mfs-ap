19a24bce694e11bbcc84fcf6d3de92f733a6db99
ataher83 <taher83@gmail.com>
2024-07-16 20:28:57 +0600

    installed axios react-router-dom jwt-decode for clientside

---
+++ ---ProjectStructure.js
@@ -0,0 +1,31 @@
+mfs-app/
+│
+├── server/
+│   ├── config/
+│   │   ├── db.js
+│   │   └── userModel.js
+│   ├── controllers/
+│   │   └── user.js
+│   ├── middlewares/
+│   │   └── auth.js
+│   ├── routes/
+│   │   └── user.js
+│   ├── server.js
+│   └── package.json
+│
+└── client/
+    ├── public/
+    │   └── index.html
+    ├── src/
+    │   ├── components/
+    │   ├── context/
+    │   │   └── AuthContext.js
+    │   ├── pages/
+    │   │   ├── Dashboard.js
+    │   │   └── Login.js
+    │   ├── utils/
+    │   │   └── PrivateRoute.js
+    │   ├── App.js
+    │   ├── index.css
+    │   └── index.js
+    └── package.json
